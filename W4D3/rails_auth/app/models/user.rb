d98b2d64945169059703b92bf7222128e59d4d58
Rayyan <rmahar@ucsd.edu>
2019-01-31 08:33:07 -0800

    Add homework for W4D3

--- W4D3/rails_auth/app/models/user.rb
+++ W4D3/rails_auth/app/models/user.rb
@@ -7,30 +7,30 @@ class User < ApplicationRecord
 
 attr_reader :password
 
-def self.find_by_credentials(username, password)
-  user = User.find_by(username: username)
-  return user if user && 
-  BCrypt::Password.new(user.password_digest).is_password?(password)
-  nil   #Return nil if user doesn't match 
-end
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && 
+    BCrypt::Password.new(user.password_digest).is_password?(password)
+    nil   #Return nil if user doesn't match 
+  end
 
-def self.generate_session_token
-  SecureRandom::urlsafe_base64(16)
-end
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
 
-def reset_session_token!
-  self.session_token = User.generate_session_token
-  self.save!
-  self.session_token
-end
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
 
-def ensure_session_token
-  self.session_token ||= User.generate_session_token
-end
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 
-def password=(password)
-  @password = password
-  self.password_digest = BCrypt::Password.create(password)
-end
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
 end
